e47fd68c699271b5bcd31a2245c8fcdfecea268f
khatridikshant <khatridikshant@outlook.com>
2024-06-24 10:50:45 +0545

    RazorPages Begins

---
+++ AspRazorPagesSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AspRazorPagesSolution.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
